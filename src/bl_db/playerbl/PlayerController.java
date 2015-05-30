a4e8874bbf42b88c311f163eb5612334a4232891
wzce <2393147618@qq.com>
2015-05-30 11:01:14 +0800

    继续写数据库的部分，球员模块

--- src/bl_db/playerbl/PlayerController.java
+++ src/bl_db/playerbl/PlayerController.java
@@ -11,6 +11,7 @@ import VO.PlayerInfoVO;
 import VO.PlayerSeasonDataVO;
 import VO.SingleMatchPersonalDataVO;
 import businessService.blservice.PlayerBLService;
+import businesslogic.bl.center.HotSort;
 
 /*
  * 使用数据库的逻辑层
@@ -23,16 +24,18 @@ public class PlayerController implements PlayerBLService{
 	String url="jdbc:mysql://localhost/mysql";
     String user="ghl";
     String pwd="ghl13";		
+    
+    String currentSeason;
 	
+	ArrayList<PlayerInfoVO> infoList;
 	
-	@Override
-	public ArrayList<PlayerInfoVO> getAllPlayerInfo() {
-	/*
-	 * 用于获取当前时间下球员最新的基本信息
-	 * 
-	 * */
-		ArrayList<PlayerInfoVO> infoList=new ArrayList<>();
-		 try {
+	public PlayerController(){
+		readBasicInfo();
+		currentSeason=getCurrentSeason();
+	}
+	
+	private String getCurrentSeason(){
+		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException | IllegalAccessException
 				| ClassNotFoundException e) {
@@ -45,14 +48,11 @@ public class PlayerController implements PlayerBLService{
 			Connection conn;
 			conn = DriverManager.getConnection(url,user, pwd);
 			stmt = conn.createStatement(); 
-			String str="select * from playerinfo";
+			String str="select MAX(season) as max from matchinfo";
 			ResultSet  rs=stmt.executeQuery(str);
-			char chr=39;
+		 
 			while(rs.next()){
-				infoList.add(new PlayerInfoVO(rs.getString("name").replace('’', chr),rs.getString("number"),
-	    			  rs.getString("position"),rs.getString("height"),
-	    			  rs.getDouble("weight"),rs.getString("birth"),
-	    			  rs.getInt("age"),rs.getInt("exp"),rs.getString("school").replace('’', chr)));
+				return rs.getString("max");
 			}
 			  stmt.close();
 		      conn.close();//使用完后就关闭数据库
@@ -60,13 +60,52 @@ public class PlayerController implements PlayerBLService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}//
+		
+		return "14-15";/*默认情况下*/
+	}
 	
+	private void readBasicInfo(){
+		/*
+		 * 用于获取当前时间下球员最新的基本信息
+		 * 
+		 * */
+			ArrayList<PlayerInfoVO> infoList=new ArrayList<>();
+			 try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+			} catch (InstantiationException | IllegalAccessException
+					| ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	      
+	         Statement stmt;
+			try {
+				Connection conn;
+				conn = DriverManager.getConnection(url,user, pwd);
+				stmt = conn.createStatement(); 
+				String str="select * from playerinfo";
+				ResultSet  rs=stmt.executeQuery(str);
+				char chr=39;
+				while(rs.next()){
+					infoList.add(new PlayerInfoVO(rs.getString("name").replace('’', chr),rs.getString("number"),
+		    			  rs.getString("position"),rs.getString("height"),
+		    			  rs.getDouble("weight"),rs.getString("birth"),
+		    			  rs.getInt("age"),rs.getInt("exp"),rs.getString("school").replace('’', chr)));
+				}
+				  stmt.close();
+			      conn.close();//使用完后就关闭数据库
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//
+	}
+	
+	
+	@Override
+	public ArrayList<PlayerInfoVO> getAllPlayerInfo() {
 		return infoList;
 	}
-
-
 	
-
 	@Override
 	public ArrayList<PlayerSeasonDataVO> getSeasonHotPlayer(String season,
 			String sortItem) {
@@ -82,8 +121,8 @@ public class PlayerController implements PlayerBLService{
 
 	@Override
 	public ArrayList<PlayerSeasonDataVO> keyfind(String text) {
-		// TODO Auto-generated method stub
 		return null;
+		
 	}
 
 	@Override
@@ -91,7 +130,39 @@ public class PlayerController implements PlayerBLService{
 			String teamAbb) {
 		ArrayList<PlayerInfoVO> list=new ArrayList<>();
 		
-		return null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      
+         Statement stmt;
+		try {
+			Connection conn;
+			conn = DriverManager.getConnection(url,user, pwd);
+			stmt = conn.createStatement(); 
+			String str="SELECT name,number,position,height,weight,birth,age,exp,school"
+					+ " FROM playerinfo,playerseasondata WHERE playerseasondata.teamAbb= '"+teamAbb+"' "
+							+ "AND playerseasondata.season='"+season+"' playerinfo.name=playerseasondata.name";
+			ResultSet  rs=stmt.executeQuery(str);
+			char chr=39;
+			while(rs.next()){
+				list.add(new PlayerInfoVO(rs.getString("name").replace('’', chr),rs.getString("number"),
+	    			  rs.getString("position"),rs.getString("height"),
+	    			  rs.getDouble("weight"),rs.getString("birth"),
+	    			  rs.getInt("age"),rs.getInt("exp"),rs.getString("school").replace('’', chr)));
+			}
+			  stmt.close();
+		      conn.close();//使用完后就关闭数据库
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}//
+		
+		
+		return list;
 	}
 
 	@Override
@@ -206,7 +277,8 @@ public class PlayerController implements PlayerBLService{
 			e.printStackTrace();
 		}//
 		
-				return list;
+		HotSort sort=new HotSort();
+		return sort.hotPlayer_Sort(list,"pointNum");/*默认以得分排序，返回一个赛季的球员的数据*/
 	}
 	
 	
@@ -220,8 +292,13 @@ public class PlayerController implements PlayerBLService{
 
 	@Override
 	public ArrayList<PlayerInfoVO> getPlayerInfoByFirstChar(String firstchar) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<PlayerInfoVO> list=new ArrayList<>();
+		for(int i=0;i<infoList.size();i++){
+			if(list.get(i).getName().startsWith(firstchar)){
+				list.add(infoList.get(i));
+			}
+		}	
+		return list;
 	}
 
 	
@@ -229,31 +306,17 @@ public class PlayerController implements PlayerBLService{
 	@Override
 	public ArrayList<PlayerSeasonDataVO> sort(String season, String type,
 			String position, String partition, String item) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<PlayerSeasonDataVO> list=getAllPlayerSeasonData(season,type);
+		list=sort_position(list,position);
+		list=sort_partition(list,partition);
+		HotSort sort=new HotSort();
+		return sort.hotPlayer_Sort(list,item);
 	}
 
 	@Override
 	public PlayerSeasonDataVO getAPlayerSeasonMatch(String season, String type,
 			String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<PlayerSeasonDataVO> sort(String season, String type,
-			String position, String partition, String[] condition,
-			boolean[] reverse) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-
-	@Override
-	public ArrayList<PlayerInfoVO> getPlayerInfo(String name) {
-		ArrayList<PlayerInfoVO> list=new ArrayList<>();
+		 
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException | IllegalAccessException
@@ -264,28 +327,120 @@ public class PlayerController implements PlayerBLService{
       
          Statement stmt;
 		try {
-			char chr=39;
 			Connection conn;
 			conn = DriverManager.getConnection(url,user, pwd);
 			stmt = conn.createStatement(); 
-			String str="select * from playerinfo WHERE name='"+name.replace(chr,'’')+"'";
+			String str="SELECT name,division,partition,position,teamAbb,SUM(matchNum) as match_num, "
+					+ "SUM(startingNum) as start_num,SUM(time) as time_sum,"
+					+ "SUM(fieldGoal) as fieldGoal_sum,SUM(shootNum) as shoot_sum,"
+					+ "SUM(t_fieldGoal) as t_fieldGoal_sum,SUM(t_shootNum)as t_shoot_sum,"
+					+ "SUM(freeThrowGoalNum) as freeThrowGoal_sum,SUM(freeThrowNum) as freeThrow_sum,"
+					+ "SUM(o_ReboundNum) as o_rebound_sum,SUM(d_reboundNum)as d_rebound_sum,"
+					+ "SUM(assistNum)as assist_sum,SUM(stealNum) as steal_sum,"
+					+ "SUM(reboundNum)as rebound_sum,SUM(blockNum)as block-sum,"
+					+ "SUM(tunrnoverNum) as turnover_sum,SUM(foulNum)as foul_sum,"
+					+ "SUM(pointNum)as point_sum,"
+					+ "AVG(efficiency) as eff,AVG(blockEfficiency)as blockEff,"
+					+ "AVG(assistEfficiency)as assistEff,AVG(reboundEfficiency) as reboundEff,"
+					+ "AVG(o_reboundEfficiency) as o_reboundEff,AVG(d_reboundEfficiency) as d_reboundEff,"
+					+ "AVG(stealEfficiency) as stealEff,AVG(usingPercentage) as usingPct,"
+					+ "AVG(blockEfficiency) as blockEff,"
+					+ "SUM(seasonDoubleNum) as double_sum,SUM(seasonThreeNum) as three_sum "
+					+ "FROM player_season_data where name='"+name+"' GROUP BY season,type,name";
 			ResultSet  rs=stmt.executeQuery(str);
-			
+			char chr=39;
 			while(rs.next()){
-				list.add( new PlayerInfoVO(rs.getString("name").replace('’', chr),rs.getString("number"),
-	    			  rs.getString("position"),rs.getString("height"),
-	    			  rs.getDouble("weight"),rs.getString("birth"),
-	    			  rs.getInt("age"),rs.getInt("exp"),rs.getString("school").replace('’', chr)));
+				return new PlayerSeasonDataVO(season,rs.getString("name"),null,
+						rs.getString("teamAbb"),rs.getString("division"),
+						rs.getString("partition"),rs.getString("position"),
+						rs.getInt("match_sum"),rs.getInt("starting_sum"),
+						rs.getDouble("time_sum"),rs.getInt("fieldGoal_sum"),
+						rs.getInt("shoot_sum"),rs.getInt("t_fieldGoal_sum"),
+						rs.getInt("t_shoot_sum"),rs.getInt("freeThrowGoal_sum"),
+						rs.getInt("freeThrow_sum"),rs.getInt("o_rebound_sum"),
+						rs.getInt("d_rebound_sum"),rs.getInt("rebound_sum"),
+						rs.getInt("assist_sum"),rs.getInt("steal_sum"),
+						rs.getInt("block_sum"),rs.getInt("turnover_sum"),
+						rs.getInt("foul_sum"),rs.getInt("point_sum"),
+						rs.getDouble("assistEff"),rs.getDouble("reboundEff"),
+						rs.getDouble("o_reboundEff"),rs.getDouble("d_reboundEff"),
+						rs.getDouble("stealEff"),rs.getDouble("usingPct"),
+						rs.getDouble("blockEff"),rs.getInt("double_sum"),rs.getInt("three_sum"),
+						null);
 			}
-			
-			
 			  stmt.close();
 		      conn.close();//使用完后就关闭数据库
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}//name,"??","??","??","??", "??", 0, 0, "??"
-		//return new PlayerInfoVO(name, "??","??","??", 0,"??", 0, 0, "??");
+		}//
+		return new PlayerSeasonDataVO(season, name, null, "??", "??", "??", "??",
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, null);/* 当在数据库中没有查询到的情况下  ,返回一个表示未知的数据*/
+	}
+
+	
+	/******************************************
+	 * 
+	 *************************************/
+	private  ArrayList<PlayerSeasonDataVO> sort_division(ArrayList<PlayerSeasonDataVO>list,
+			String division) {
+		 /*按联盟筛选*/
+		ArrayList<PlayerSeasonDataVO> vo=new ArrayList<>();
+		for(int i=0;i<list.size();i++){
+			if(list.get(i).getDivision().equals(division)){
+			vo.add(list.get(i));
+			}
+		}
+		return vo;
+		}
+		
+	 
+	private ArrayList<PlayerSeasonDataVO> sort_partition(ArrayList<PlayerSeasonDataVO>list,
+			String partition) {
+		 
+		ArrayList<PlayerSeasonDataVO> vo=new ArrayList<>();
+		for(int i=0;i<list.size();i++){
+			if(list.get(i).getPartition().equals(partition)){
+			vo.add(list.get(i));
+			}
+		}
+		return vo;
+	}
+	
+	private ArrayList<PlayerSeasonDataVO> sort_position(ArrayList<PlayerSeasonDataVO>list,String position){
+		/*这个地方有待思考*/
+		ArrayList<PlayerSeasonDataVO> vo=new ArrayList<>();
+		for(int i=0;i<list.size();i++){
+			if(list.get(i).getInfo().getPosition().contains(position)){
+			vo.add(list.get(i));
+			}
+		}
+		return vo;
+		 
+	}
+	
+
+	@Override
+	public ArrayList<PlayerSeasonDataVO> sort(String season, String type,
+			String position, String partition, String[] condition,
+			boolean[] reverse) {
+		ArrayList<PlayerSeasonDataVO> list=getAllPlayerSeasonData(season,type);
+		list=sort_position(list,position);
+		list=sort_partition(list,partition);
+		HotSort sort=new HotSort();
+		return sort.hotPlayer_Sort(list, condition, reverse);
+	}
+
+
+	@Override
+	public ArrayList<PlayerInfoVO> getPlayerInfo(String name) {
+		ArrayList<PlayerInfoVO> list=new ArrayList<>();
+		for(int i=0;i<infoList.size();i++){
+			if(infoList.get(i).getName().equals(name)){
+				list.add(infoList.get(i));
+			}
+		}
 		return list;
 		 //当数据库中没有对应球员的基础数据时，返回一个表示未知的信息
 	}
